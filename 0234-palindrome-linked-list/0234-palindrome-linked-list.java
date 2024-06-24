8388629e0a3eab1213da66a982bc2e9b3c83a6f1
MADANNAGARI RAJU <106908371+MadannagariRaju@users.noreply.github.com>
2024-06-24 11:27:33 +0530

    Time: 4 ms (85.96%), Space: 68.9 MB (19.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode temp = head;
+        /* ListNode temp = head;
         int size=0;
         Stack<Integer>st = new Stack<>();
         while(temp !=null) {
@@ -24,8 +24,46 @@ class Solution {
             if(temp.val != data) return false;
             temp = temp.next;
         }
-        return true;
+        return true;   */
 
-        
+        /*
+        1.Find out the middle Node
+        2.reverse the second part
+        3.compare the first and second part
+         */
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next !=null && fast.next.next !=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode newhead  = reverse(slow.next);
+
+        ListNode first = head;
+        ListNode second = newhead;
+        while(second != null) // second part last node points to null
+        {
+            if(first.val != second.val) {
+                reverse(newhead);  // make the altered data to the original data
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        reverse(newhead);
+        return true;
+    }
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode temp = head;
+        while(temp != null) {
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
     }
 }
